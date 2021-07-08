a493b7e329086c5b8d48127e34a31d573cc006fe
DevangCMishra <devangpro2925@gmail.com>
2021-07-08 15:09:55 +0200

    Add project files.

---
+++ DemoWebApp/Controllers/HelloController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DemoWebApp.Controllers
+{
+    [Route("hello")]
+    public class HelloController : ApiController
+    {
+       [HttpGet]
+       public string Get()
+        {
+            return("Hello World");
+        }
+        // GET: api/Hello
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET: api/Hello/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Hello
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Hello/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Hello/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DemoWebApp/Controllers/PeopleController.cs
@@ -0,0 +1,51 @@
+﻿using DemoWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DemoWebApp.Controllers
+{
+    public class PeopleController : ApiController
+    {
+        List<Person> people = new List<Person>();
+
+        public PeopleController()
+        {
+            people.Add(new Person { FirstName = "Tim", LastName = "Larry", Id = 1 });
+            people.Add(new Person { FirstName = "Kim", LastName = "Larry", Id = 2 });
+            people.Add(new Person { FirstName = "Randy", LastName = "Larry", Id = 3 });
+        }
+
+        // GET: api/People
+        public List<Person> Get()
+        {
+            return people;
+        }
+
+        // GET: api/People/5
+        public Person Get(int id)
+        {
+            return people.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+
+
+        // POST: api/People
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/People/5
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        // DELETE: api/People/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
